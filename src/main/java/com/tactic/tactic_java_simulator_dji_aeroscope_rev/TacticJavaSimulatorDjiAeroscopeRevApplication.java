a32db8e760aea8ac048ece515c62a7ce445cd725
kapich1192 <kapich1192@yandex.ru>
2023-11-05 21:06:39 +0300

    init: First commit

---
+++ src/main/java/com/tactic/tactic_java_simulator_dji_aeroscope_rev/TacticJavaSimulatorDjiAeroscopeRevApplication.java
@@ -0,0 +1,13 @@
+package com.tactic.tactic_java_simulator_dji_aeroscope_rev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacticJavaSimulatorDjiAeroscopeRevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacticJavaSimulatorDjiAeroscopeRevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tactic/tactic_java_simulator_dji_aeroscope_rev/TacticJavaSimulatorDjiAeroscopeRevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactic.tactic_java_simulator_dji_aeroscope_rev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacticJavaSimulatorDjiAeroscopeRevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
